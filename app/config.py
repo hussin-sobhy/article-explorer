ceb3ce93aad6dae146b27ea841c598ec795fb426
hussinxx700 <hussinsobhy700@gmail.com>
2025-09-12 04:25:48 +0300

    Update config to include the MongoDB connection {URL, DB, Collection}

--- app/config.py
+++ app/config.py
@@ -4,6 +4,9 @@ class Settings(BaseSettings):
     Qdrant_url: str = "http://localhost:6333"
     GROQ_API_KEY: str
     FIRECRAWL_API_KEY: str
+    mongodb_uri: str = ""
+    mongodb_db: str = "article_explorer"
+    mongodb_collection: str = "chunks"
 
     class Config:
         env_file = ".env"
